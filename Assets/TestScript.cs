320ed9aa766c5e1ce62ce8d8335feb1486692a5c
Demid Lupin <lupin@demid.su>
2020-05-01 12:35:39 +0300

    Sample

---
+++ Assets/TestScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update start");
+        Foo();
+        Debug.Log("Update end");
+    }
+
+    void Foo()
+    {
+        Debug.Log("Foo start");
+        Bar();
+        Debug.Log("Foo end");
+    }
+
+    void Bar()
+    {
+        Debug.Log("Bar");
+    }
+}
